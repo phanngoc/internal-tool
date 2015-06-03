6053ed6e220b3ae852b52fd62469c6df8ff3a9b3
phanngoc <lequidon.1993@gmail.com>
2015-06-03 16:14:08 +0700

    them 3 model project,projectstatus,timesheet

---
+++ app/Timesheet.php
@@ -0,0 +1,30 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Timesheet extends Model {
+
+	protected $table = 'timesheets';
+
+	protected $fillable = [
+		'taskname',
+		'start',
+		'end',
+		'duration',
+		'project_id',
+		'status_id',
+		'comments',
+		;
+	];
+
+	public function project()
+	{
+		return $this->belongsTo('App\Project','project_id','id');
+	}
+
+	public function projectstatus()
+	{
+		return $this->belongsTo('App\Projectstatus','status_id','id');
+	}
+
+}
