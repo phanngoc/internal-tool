5d9ed45deb927b9b9e5838c68f8ea4c6c1d392b8
thiet phan <ducthietk16@gmail.com>
2015-06-12 11:29:26 +0700

    model employee

---
+++ app/Education.php
@@ -0,0 +1,25 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Education extends Model {
+
+	protected $table = 'educations';
+
+	protected $fillable = [
+		'id',
+		'year_start',
+		'employee_id',
+		'year_end',
+		'description',
+		'certificate',
+		
+		
+	];
+
+	public function employee(){
+		return this->belongsTo('App\Employee');
+
+	}
+
+}
---
+++ app/Working_experience.php
@@ -0,0 +1,28 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Working_experiences extends Model {
+
+	protected $table = 'working_experiences';
+
+	protected $fillable = [
+		'id',
+		'project_name',
+		'employee_id',
+		'role',
+		'company',
+		'main_duties',
+		'project_period',
+		'skill_set_ultilized',
+		'year_start',
+		'year_end',
+		
+	];
+
+	public function employee(){
+		return this->belongsTo('App\Employee');
+
+	}
+
+}
