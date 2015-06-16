a6b3bbeaa193878858b275583b856fc2d7cdd2b8
vanthinh91 <vanthinh991@gmail.com>
2015-06-16 10:20:12 +0700

    skills

--- resources/views/profiles/profiles.blade.php
+++ resources/views/profiles/profiles.blade.php
@@ -415,8 +415,7 @@
 
                       <h3>{{ trans('messages.skills') }}</h3>
                       <div>
-                          <p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis.</p>
-                          <p>Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
+                        @include('profiles.skill')
                       </div>
 
                       <h3>{{ trans('messages.educations') }}</h3>
---
+++ resources/views/profiles/skill.blade.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" type="text/css" href="{{Asset('css/jsgrid.css')}}" />
+<link rel="stylesheet" type="text/css" href="{{Asset('css/theme.css')}}" />
+<link rel="stylesheet" type="text/css" href="{{Asset('css/jquery-ui.css')}}" />
+<link href="{{Asset('bootstrap/css/select2.min.css')}}" rel="stylesheet" type="text/css" />
+<div id="jsGridProject"></div>
+{{ $employee->id }}
