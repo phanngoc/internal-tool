c61a456006f12a2b4066a681ccc80fd6c9b5b3a3
Nguyen Thanh Vu <thanhvunguyenbkdn@gmail.com>
2015-06-18 11:44:26 +0700

    thanhvu

--- app/Http/Controllers/PositionController.php
+++ app/Http/Controllers/PositionController.php
@@ -1,13 +1,10 @@
 <?php namespace App\Http\Controllers;
 use App;
-use App\Http\Controllers\Controller;
-use App\Http\Requests\PositionRequest;
-use Illuminate\Support\Facades\Redirect;
+use App\Position;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Request;
-use App\Employee;
-use App\Position;
 use Illuminate\Support\Facades\Validator;
+
 class PositionController extends AdminController {
 
 	/**
@@ -21,78 +18,71 @@ class PositionController extends AdminController {
 	}
 
 	public function listposition() {
-		$position=Position::all();
-			$response = array();
+		$position = Position::all();
+		$response = array();
 		foreach ($position as $key => $value) {
 			$item = array("id" => $value->id,
 				"name" => $value->name,
-				"description" => $value->description,	
+				"description" => $value->description,
 			);
 
 			array_push($response, $item);
 		}
 		return json_encode($response);
-		
 
 	}
 
-	public function insert(){
+	public function insert() {
 
-		$position=new Position;
+		$position = new Position;
 
-		$position->name=Request::input('name');
-		$position->description=Request::input('description');
-	    $validator = Validator::make(
-		    [
-		      'name' => $position->name
+		$position->name = Request::input('name');
+		$position->description = Request::input('description');
+		$validator = Validator::make(
+			[
+				'name' => $position->name,
 
-		    ],
-		    ['name' => ['required', 'min:5']]
+			],
+			['name' => ['required', 'min:5']]
 		);
 
+		if ($validator->fails()) {
+			//return  $validator->messages()->toJson();
+			//return Response::json($validator->messages(), 400);
 
-		if ($validator->fails())
-		 {
-	   //return  $validator->messages()->toJson();
-		//return Response::json($validator->messages(), 400);
-		
-         if (Request::ajax())
-            {
-                return "";
-            }
-		 }
+			if (Request::ajax()) {
+				return "";
+			}
+		}
 
 		$position->save();
 		echo json_encode($position);
 	}
 
+	public function updateposition() {
+		$validator = Validator::make(
+			[
+				'name' => Request::input('name'),
 
-	public function updateposition(){
-		 $validator = Validator::make(
-		    [
-		      'name' => Request::input('name')
-
-		    ],
-		    ['name' => ['required', 'min:5','alpha']]
+			],
+			['name' => ['required', 'min:5']]
 		);
-if ($validator->fails())
-		 {
-	   //return  $validator->messages()->toJson();
-		//return Response::json($validator->messages(), 400);
-		
-         if (Request::ajax())
-            {
-              return  "";
-            }
-		 }
-		$position= Position::find(Request::input('id'));
-		   $position->update([
-		'name' => Request::input('name'),
-		'description' => Request::input('description')]);
+		if ($validator->fails()) {
+			//return  $validator->messages()->toJson();
+			//return Response::json($validator->messages(), 400);
+
+			if (Request::ajax()) {
+				return "";
+			}
+		}
+		$position = Position::find(Request::input('id'));
+		$position->update([
+			'name' => Request::input('name'),
+			'description' => Request::input('description')]);
 		$item = array("id" => $position->id,
-					 "name" => $position->name,
-					"description" => $position->description);
-		echo  json_encode($item);
+			"name" => $position->name,
+			"description" => $position->description);
+		echo json_encode($item);
 	}
 
 	public function destroy() {
@@ -100,8 +90,8 @@ if ($validator->fails())
 		$position = Position::find(Request::input('id'));
 		$position->delete();
 		$item = array("id" => $position->id,
-					 "name" => $position->name,
-					"description" => $position->description);
+			"name" => $position->name,
+			"description" => $position->description);
 
 		echo json_encode($item);
 	}
