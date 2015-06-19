0677848d75edab43b1fb370ea0fd196085a3e8df
thiet phan <ducthietk16@gmail.com>
2015-06-19 11:15:46 +0700

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,9 @@
     xmlns="http://www.w3.org/TR/REC-html40">
     <head>
       <title>Print Preview  Page</title>
-      
+      <link rel="stylesheet" href="{!!Asset('bootstrap/css/print-preview.css')!!}" type="text/css" media="screen">
+      <script src="{!!Asset('bootstrap/js/jquery.tools.min.js')!!}"></script>
+      <script src="{!!Asset('bootstrap/js/jquery.print-preview.js')!!}" type="text/javascript" charset="utf-8"></script>
       <link rel=File-List href="{!!Asset('cv/filelist.xml')!!}">
       <link rel=Edit-Time-Data href="{!!Asset('cv/editdata.mso')!!}">
       <link rel=dataStoreItem href="{!!Asset('cv/item0001.xml')!!}"
@@ -15,7 +17,7 @@
       <link rel=themeData href="{!!Asset('cv/themedata.thmx')!!}">
       <link rel=colorSchemeMapping
         href="{!!Asset('cv/colorschememapping.xml')!!}">
-  
+    
     </head>
     <body lang=EN-US link=blue vlink=purple style='tab-interval:.5in'>
       <div style='margin:0px 120px'id="wrapper">
@@ -142,8 +144,11 @@
                       <v:imagedata src="CV_DinhThiLoan_%20AsianTech_files/image001.png" o:title=""/>
                     </v:shape>
                     <![endif]--><![if !vml]><img width=78 height=100
-                      src="{!!Asset('cv/image002.png')!!}" v:shapes="Picture_x0020_3"><![endif]>
+                      src="{!!Asset('cv/'.$employee->avatar)!!}" v:shapes="Picture_x0020_3"><![endif]>
                   </span>
+                  <?php
+
+                  ?>
                   <span
                     style='font-size:10.0pt;font-family:"Arial","sans-serif"'>
                     <o:p></o:p>
@@ -426,7 +431,7 @@ foreach ($educations as $key => $value) {
               </td>
             </tr>
             <?php
-            foreach ($category_skill as $key => $value){
+            foreach ($category_skill as $key => $value1){
               ?>
 
             <tr style='mso-yfti-irow:1'>
@@ -436,7 +441,7 @@ foreach ($educations as $key => $value) {
                 <p class=MsoNormal style='margin-top:6.0pt;margin-right:0in;margin-bottom:
                   6.0pt;margin-left:0in'>
                   <span style='font-size:10.0pt;font-family:"Arial","sans-serif"'>
-                   {!!$value->category_name!!}
+                   {!!$value1->category_name!!}
                     <span style='background:yellow;mso-highlight:yellow'>
                       <o:p></o:p>
                     </span>
@@ -453,10 +458,15 @@ foreach ($educations as $key => $value) {
                   <span style='font-size:10.0pt;font-family:"Arial","sans-serif"'>
                    <?php
                    foreach ($employee_skills as $key => $value) {
-                    ?>
-                        {!!$value->skill!!}
+                    if ($value->skill->category_id==$value1->id){
+                    ?>  
+                        
+                        {!!$value->skill->skill!!}({!!$value->month_experience!!} tháng),
 
-                   <?php }
+                   
+
+                   <?php }}
+
                    ?>
                   
                     
