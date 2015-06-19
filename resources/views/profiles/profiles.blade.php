a7cdbe3f4bb91805ad376c2b76480f161b2f123b
phanngoc <lequidon.1993@gmail.com>
2015-06-19 08:21:39 +0700

    ok ne

--- resources/views/profiles/profiles.blade.php
+++ resources/views/profiles/profiles.blade.php
@@ -224,7 +224,6 @@
                   <h3 class="box-title">{{trans('messages.profile')}}</h3>
                 </div>
                 <div class="box-body">
-                  <form action="{{ route('employee.editmore.store', $employee->id) }}" method="POST" id="formprofile">
                   <form action="{{ route('profiles.store') }}" method="POST" id="formprofile">
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                     <input type="hidden" name="imageup"/>
